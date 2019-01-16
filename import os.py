ba6e4b23117804fac27da2c5cf1e1fd2636f9a8c
Piyush <tiwaripiyush25@gmail.com.com>
2019-01-16 19:21:16 +0530

    16 jan 2019

--- import os.py
+++ import os.py
@@ -1,16 +1,32 @@
+#import untangle
+#import os
+# obj = untangle.parse('mfg_fix1.xml')
+
+# print (obj.foo.bar['name'])
+# print (obj.foo.bar.type['foobar'])
+# obj.foo.bar.attributes={'name': "hshsh"}
+#print (os.path.dirname('mfg_fix3'))ixing
+
 import os 
 import xml.etree.ElementTree as Et
-file_name='mfg_fix.xml'
-full_file=os.path.abspath(os.path.join('data',file_name)) #location offile
-tree = Et.parse(full_file)
+file_name='MFG.xml'
+base_path = os.path.dirname(os.path.realpath(__file__))
+xml_file = os.path.join(base_path,file_name)
+print('xml_file ---->',xml_file)
+tree = Et.parse("C:/Users/admin/Desktop/Django_Project/tennis/data/MFG.xml")
 root = tree.getroot()
+count=0
 for child in root:         #loops for each child
+	count+=1
+	child.attrib = {'pk':str(count),'model':'Meddata.medicine'}
+	child.tag = "object"
 	for element in child:   #loops for element is child
-		element.tag = "field" #tags names will b cahnged to it
-tree.write(full_file)        # to write the changes
-
-
-        
-    
-
-
+		#tags names will b cahnged to it
+		element.attrib= {"name":element.tag}
+		if (element.tag)=="COCD":
+			element.attrib={"type":"PositiveInteger"}
+		if (element.tag)=="NAME" or "Address" or "Phone":
+			element.attrib={"type":"Charfield"}
+		element.tag = "field"
+		print(element.tag,"------->",element.text)  
+tree.write(xml_file)      # to write the change
